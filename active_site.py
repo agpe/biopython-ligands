acd2a89ac6c4004a4f7c3fef589e9052c9a87cd7
agpe <agpe@users.noreply.github.com>
2013-11-29 16:17:53 +0100

    Update active_site.py

--- active_site.py
+++ active_site.py
@@ -60,3 +60,19 @@ def save_ligand(structure, filename):
     io=PDBIO()
     io.set_structure(structure)
     io.save(filename+'.pdb', LigandSelect())
+
+"""
+Example
+pdb_code = "1E8W"
+pdb_filename = "1E8W.pdb"
+structure = Bio.PDB.PDBParser().get_structure(pdb_code, pdb_filename)
+model = structure[0]
+ligand = get_ligand_by_name('QUE', model)
+active_site(ligand, 5.0, model)
+
+structure2 = Bio.PDB.PDBParser().get_structure('4GRV', '4GRV.pdb')
+model2 = structure2[0]
+ligand2 = get_ligand_by_chain('B', model2)
+active_site(ligand2, 8.0, model2)
+save_ligand(structure2, 'ligand')
+"""
